032e33f63a915aec58900829f2f03ace0cf7d14d
MohsenDeniku <mohsendeniku@gmail.com>
2024-10-04 22:29:01 +0330

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let homeScore = document.getElementById("home-score").textContent
+let guestScore = document.getElementById("guest-score").textContent
+function addScore(team, score) {
+    if (team == 'home') {
+        homeScore = parseInt(homeScore) + score
+        document.getElementById("home-score").innerText = homeScore
+    } else if (team == 'guest') {
+        guestScore = parseInt(guestScore) + score
+        document.getElementById("guest-score").innerText = guestScore
+    }
+}
+
+function resetBtn() {
+    document.getElementById("guest-score").innerText = 0
+    document.getElementById("home-score").innerText = 0
+    homeScore = 0
+    guestScore = 0
+    document.getElementById('winner').textContent = ''
+    document.getElementById('home-1').disabled = false
+    document.getElementById('home-2').disabled = false
+    document.getElementById('home-3').disabled = false
+    document.getElementById('guest-1').disabled = false
+    document.getElementById('guest-2').disabled = false
+    document.getElementById('guest-3').disabled = false
+}
+
+function announceWinner() {
+    if (homeScore > guestScore) {
+        document.getElementById('winner').textContent = 'Team Home is the winner'
+    } else if (homeScore < guestScore) {
+        document.getElementById('winner').textContent = 'Team Guest is the winner'
+    } else {
+        document.getElementById('winner').textContent = 'Draw'
+    }
+    document.getElementById('home-1').disabled = true
+    document.getElementById('home-2').disabled = true
+    document.getElementById('home-3').disabled = true
+    document.getElementById('guest-1').disabled = true
+    document.getElementById('guest-2').disabled = true
+    document.getElementById('guest-3').disabled = true
+}
